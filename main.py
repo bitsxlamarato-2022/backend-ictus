2937d7423d26ccc3189feda6aa7cc86f91cc30f5
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2022-12-18 03:34:02 +0100

    notebooks

--- main.py
+++ main.py
@@ -1,38 +1,17 @@
 from fastapi import FastAPI
 
 from models import *
-from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
-import sqlalchemy
 
-import fitbit
-import gather_keys_oauth2 as Oauth2
 import pandas as pd
 
 CLIENT_ID = '238RX8'
 CLIENT_SECRET = 'dff37bf9cb0059dfc910129162e035ba'
+HOST = "https://api.fitbit.com"
 
-server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
-# from oauthlib.oauth2 import WebApplicationClient
-# WebApplicationClient()
-server.browser_authorize()
-ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
-REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
-auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN,
-                             refresh_token=REFRESH_TOKEN)
-
-oneDate = pd.datetime(year=2002, month=12, day=16)
-oneDayData = auth2_client.intraday_time_series('activities/heart', oneDate, detail_level='1sec')
-df = pd.DataFrame(oneDayData['activities-heart-intraday']["dataset"])
-
-print(df)
-df.to_csv('HRdata.csv')
-""""""
 
 app = FastAPI()
 
 users = {}
-
 
 @app.post("/register")
 async def register(user: User):
