6d5ed63f831e5d61855705590be6500b4792c157
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2022-12-18 09:42:58 +0100

    added endpoint

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ import pandas as pd
 import json
 import requests
 import scipy.io as io
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+import sqlalchemy
+
+import fitbit
+import gather_keys_oauth2 as Oauth2
 
 import requests
 
@@ -15,13 +21,22 @@ CLIENT_ID = '238RX8'
 CLIENT_SECRET = 'dff37bf9cb0059dfc910129162e035ba'
 HOST = "https://api.fitbit.com"
 
+CLIENT_ID = '23923K'
+CLIENT_SECRET = 'e75bf5bf8e57aac3689717bad636cb97'
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN,
+                             refresh_token=REFRESH_TOKEN)
+auth2_client.make_request(
+    'https://api.fitbit.com/1/user/-/ecg/list.json?beforeDate=2022-09-28&sort=asc&limit=1&offset=0')
 
 app = FastAPI()
 
 origins = [
     "*"
 ]
-
 
 users = {"string": User(id="string", password="pollagorda69", name="pol", surname="escolar", age=12)}
 
@@ -32,7 +47,6 @@ async def register(user: User):
     return user
 
 
-
 @app.post("/login/")
 async def login(credentials: Credentials):
     if credentials.id not in users.keys():
@@ -41,6 +55,7 @@ async def login(credentials: Credentials):
         return False
     return True
 
+
 @app.post("/user/")
 async def edit_user(user: User):
     if user.id not in users.keys():
@@ -48,14 +63,17 @@ async def edit_user(user: User):
     users[user.id] = user
     return users
 
+
 @app.get("/users/{userid}/")
 async def get_user(userid):
     return users[userid]
+
 
 def turn_resp_to_mat(resp):
     ys = json.loads(resp.text)['ecgReadings'][0]['waveformSamples']
     dic = {'val': ys}
     io.savemat('tmp.mat', dic)
+
 
 @app.post("/ecg/{userid}/")
 async def get_ecg(userid):
@@ -64,6 +82,7 @@ async def get_ecg(userid):
     })
     turn_resp_to_mat(resp)
     return 0
+
 
 def plot_ecg(resp):
     plt.figure()
@@ -75,11 +94,12 @@ def plot_ecg(resp):
     buf.seek(0)
     return buf
 
-@app.post("/egs/{userid}/",responses = {
-        200: {
-            "content": {"image/png": {}}
-        }
-    },response_class=Response
+
+@app.post("/egs/{userid}/", responses={
+    200: {
+        "content": {"image/png": {}}
+    }
+}, response_class=Response
           )
 async def egs_image(userid):
     resp = requests.get(f"{HOST}/1/user/-/ecg/list.json?beforeDate=2022-09-28&sort=asc&limit=10&offset=0", headers={
